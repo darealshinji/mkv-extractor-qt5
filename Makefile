174b89f4290f37e538432bc2c30a71ce08be08af
darealshinji <djcj@gmx.de>
2016-04-23 23:51:21 +0200

    build single-app with embedded localizations

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all:
 
 clean:
 	./clean.sh
-	-rm -f MKVExtractorQt_new.py
+	-rm -f MKVExtractorQt_new.py tmp?.py MKVExtractorQt_2.py MKVRessources_2.qrc
 
 distclean: clean
 
--- singleapp.sh
+++ singleapp.sh
@@ -1,17 +1,26 @@
-#!/bin/sh
+#!/bin/sh -e
 
 ./build.sh
+
+sed -e 's@</qresource>@<file>MKVExtractorQt_cs_CZ.qm</file><file>MKVExtractorQt_fr_FR.qm</file></qresource>@' \
+  MKVRessources.qrc > MKVRessources_2.qrc
+pyrcc5 MKVRessources_2.qrc -o MKVRessources_rc.py
+
+sed -e 's@"MKVExtractorQt_fr_FR", str(folder)@":/MKVExtractorQt_fr_FR.qm"@' \
+    -e 's@"MKVExtractorQt_cs_CZ", str(folder)@":/MKVExtractorQt_cs_CZ.qm"@' \
+MKVExtractorQt.py > MKVExtractorQt_2.py
+
 sed '/import MKVRessources_rc/r MKVRessources_rc.py' ui_MKVExtractorQt.py > tmp1.py
-sed '/from ui_MKVExtractorQt import/r tmp1.py' MKVExtractorQt.py > tmp2.py
+sed '/from ui_MKVExtractorQt import/r tmp1.py' MKVExtractorQt_2.py > tmp2.py
 sed '/import MKVRessources_rc/d; /from ui_MKVExtractorQt import/d' tmp2.py > tmp3.py
 
-pyminifier --version 2>/dev/null >/dev/null
-if [ $? = 0 ]; then
+if pyminifier --version 2>/dev/null >/dev/null ; then
   pyminifier tmp3.py > MKVExtractorQt_new.py
 else
+  echo "no pyminifier found"
   mv tmp3.py MKVExtractorQt_new.py
 fi
 
-rm -f tmp?.py
+rm -f tmp?.py MKVExtractorQt_2.py MKVRessources_2.qrc
 chmod a+x MKVExtractorQt_new.py
 

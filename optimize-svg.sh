2e8791353c43821749b9d2fe0af9f043d6bec3e7
darealshinji <djcj@gmx.de>
2016-06-05 17:14:41 +0200

    helper script to optimize SVG

---
+++ optimize-svg.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -f mkv-extractor-qt5_optimized.svg
+scour --enable-id-stripping \
+	--shorten-ids \
+	--enable-comment-stripping \
+	--disable-embed-rasters \
+	--remove-metadata \
+	--indent=none \
+	-i mkv-extractor-qt5.svg \
+	-o mkv-extractor-qt5_optimized.svg
+

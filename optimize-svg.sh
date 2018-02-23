4c561a0d509591a218bcfac84cc888f9f08e1f80
darealshinji <djcj@gmx.de>
2018-02-23 16:31:15 +0100

    Add optimized SVG

--- optimize-svg.sh
+++ optimize-svg.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-rm -f mkv-extractor-qt5_optimized*.svg
+
 scour --enable-id-stripping \
 	--shorten-ids \
 	--enable-comment-stripping \
 	--disable-embed-rasters \
 	--remove-metadata \
-	--indent=none \
-	-i mkv-extractor-qt5.svg \
-	-o mkv-extractor-qt5_optimized_.svg
-tr -d '\n' < mkv-extractor-qt5_optimized_.svg > mkv-extractor-qt5_optimized.svg
-rm -f mkv-extractor-qt5_optimized_.svg
+	--indent=space \
+	-i mkv-extractor-qt5_no_opt.svg \
+	-o mkv-extractor-qt5.svg
+

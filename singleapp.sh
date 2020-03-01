85bac395309884b2ef688b0d5c929299b1f2a3fc
darealshinji <djcj@gmx.de>
2020-03-01 15:55:11 +0100

    Make WhatsUp usable in portable build

--- singleapp.sh
+++ singleapp.sh
@@ -7,29 +7,34 @@ set -e
 ./build.sh
 
 # add localizations and changelog to resources
-sed -e 's@</qresource>@<file>MKVExtractorQt5_cs_CZ.qm</file><file>MKVExtractorQt5_fr_FR.qm</file></qresource>@' \
+sed -e 's@</qresource>@<file>MKVExtractorQt5_cs_CZ.qm</file><file>MKVExtractorQt5_fr_FR.qm</file><file>changelog.Debian</file></qresource>@' \
   MKVRessources.qrc > tmp_MKVRessources.qrc
 pyrcc5 tmp_MKVRessources.qrc -o MKVRessources_rc.py
 
 # use embedded localizations and changelog
 sed -e 's@"MKVExtractorQt5_fr_FR", str(AppFolder)@":/MKVExtractorQt5_fr_FR.qm"@' \
     -e 's@"MKVExtractorQt5_cs_CZ", str(AppFolder)@":/MKVExtractorQt5_cs_CZ.qm"@' \
-    -e 's@/usr/share/doc/mkv-extractor-qt5/changelog.Debian.gz@:/changelog.Debian.gz@g' \
+    -e 's@/usr/share/doc/mkv-extractor-qt5/changelog.Debian.gz@:/changelog.Debian@g' \
 MKVExtractorQt5.py > tmp_MKVExtractorQt5.py
 
 # remove some import lines
 sed '/import MKVRessources_rc/d' ui_MKVExtractorQt5.py > tmp_ui_MKVExtractorQt5.py
 sed -i '/^from QFileDialogCustom/d; /^from WhatsUp/d; /^from ui_MKVExtractorQt5/d' tmp_MKVExtractorQt5.py
 
-# whatsup not yet portable
-sed -i '/changelog.Debian.gz/d; /whatsup/d; /WhatsUpTitle/d' tmp_MKVExtractorQt5.py
-sed -i '/whatsup/d' tmp_ui_MKVExtractorQt5.py
+# whatsup
+sed -i '/changelog.Debian...exists/d; /self.ui.whatsup.setVisible/d' tmp_MKVExtractorQt5.py
+echo 'changelog_data = b"\\' > tmp_changelog.py
+xxd -g1 changelog.Debian | cut -d ':' -f2 | sed -e 's|  .*||' -e 's| |\\x|g; s|$|\\|g' >> tmp_changelog.py
+echo '"' >> tmp_changelog.py
 
 # make final script usable
 awk "/if __name__ == '__main__':/ {exit} {print}" QFileDialogCustom/QFileDialogCustom.py > tmp_QFileDialogCustom.py
+awk "/if __name__ == '__main__':/ {exit} {print}" WhatsUp/WhatsUp.py > tmp_WhatsUp.py
+sed -i '/gzip.open/d' tmp_WhatsUp.py
+sed -i 's@    file_content = f.read()@file_content = changelog_data@' tmp_WhatsUp.py
 
 # merge scripts
-cat tmp_QFileDialogCustom.py tmp_ui_MKVExtractorQt5.py MKVRessources_rc.py CodecListFile.py > tmp.py
+cat tmp_changelog.py tmp_WhatsUp.py tmp_QFileDialogCustom.py tmp_ui_MKVExtractorQt5.py MKVRessources_rc.py CodecListFile.py > tmp.py
 sed -i -e '/^from CodecListFile/r tmp.py' -e '/^from CodecListFile/d' tmp_MKVExtractorQt5.py
 
 # minify script
